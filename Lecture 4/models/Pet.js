05b8cece0584c73cc9692406c5e62eaaf77ccd70
ItsY8S <gri.y8s@gmail.com>
2018-08-06 21:02:32 -0400

    Removing old files. Third try

--- Lecture 4/models/Pet.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const petSchema = mongoose.Schema({
-  name: String,
-  type: { type: String, enum: ['Dog', 'Cat'] },
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-})
-
-module.exports = mongoose.model('Pet', petSchema)
