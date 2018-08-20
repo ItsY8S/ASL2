7679c59b39b2427d7e8617eeffaa64dfa59d350a
ItsY8S <gri.y8s@gmail.com>
2018-08-19 22:21:53 -0400

    Added more routes.

--- Assignment 8/assignment8/node/routes/index.js
+++ Assignment 8/assignment8/node/routes/index.js
@@ -1,79 +1,91 @@
-const express = require('express');
+const express = require('express')
 const router = express.Router()
 const path = require('path')
-const axios = require('axios');
+const axios = require('axios')
 
 // Default page
 router.get('/', (req, res, next) => {
-    // GET DAT FEED WOWOWOW
-    async function getFeed() {
-        try {
-            const response = await axios.get('http://localhost:8000/feed', { proxy: { host: '127.0.0.1', port: 8000 } })
-            console.log(response.data);
-            res.json(response.data)
-        }
-        catch (error) {
-            console.log("Error", error);
-        }
+  // GET DAT FEED WOWOWOW
+  async function getFeed() {
+    try {
+      const response = await axios.get('http://localhost:8000/feed', {
+        proxy: { host: '127.0.0.1', port: 8000 }
+      })
+      // Log the response data
+      console.log(response.data)
+      // Respond with the response data
+      res.json(response.data)
+    } catch (error) {
+      console.log('Error', error)
     }
+  }
 
-    // Render their feed
-    getFeed()
+  // Render their feed
+  getFeed()
 })
 
 // Render all posts from a user based on their id
 router.get('/feed/:userId', (req, res) => {
-    async function getUserPosts() {
-        try {
-            const response = await axios.get('http://localhost:8000/feed/:id', { proxy: { host: '127.0.0.1', port: 8000 } })
-            console.log(response.data);
-            res.json(response.data)
-        }
-        catch (error) {
-            console.log("Error:", error);
-        }
+  // Async / await axios response
+  async function getUserPosts() {
+    // Try it. Also check for failure.
+    try {
+      // Store the response in a variable
+      const response = await axios.get('http://localhost:8000/feed/:id', {
+        proxy: { host: '127.0.0.1', port: 8000 }
+      })
+      // Log the response data
+      console.log(response.data)
+      // Respond with the data
+      res.json(response.data)
+      // Catch the error in the event of one
+    } catch (error) {
+      // Log the error
+      console.log('Error:', error)
     }
+  }
 
-    // Render the posts for a user
-    getUserPosts()
+  // Render the posts for a user
+  getUserPosts()
 })
 
-
+// New route
 router.get('/new', (req, res) => {
-    res.render('form')
+  // Reender the form view
+  res.render('form')
 })
 
 // Create a new post
 router.post('/post', (req, res) => {
-    var bodyFormData = new FormData();
-    bodyFormData.set('text', 'This is a new post');
+  var bodyFormData = new FormData()
+  bodyFormData.set('text', 'This is a new post')
 
-    axios({
-        method: 'post',
-        url: 'http://localhost:8000/post',
-        data: bodyFormData,
-        config: { headers: { 'Content-Type': 'multipart/form-data' } }
+  axios({
+    method: 'post',
+    url: 'http://localhost:8000/post',
+    data: bodyFormData,
+    config: { headers: { 'Content-Type': 'multipart/form-data' } }
+  })
+    .then(function(response) {
+      //handle success
+      console.log(response)
     })
-        .then(function (response) {
-            //handle success
-            console.log(response);
-        })
-        .catch(function (response) {
-            //handle error
-            console.log(response);
-        });
+    .catch(function(response) {
+      //handle error
+      console.log(response)
+    })
 
-    // axios.post('http://localhost:8000/post', {
-    //     "id": "5",
-    //     "text": "Test post",
-    //     "likes": "5"
-    // })
-    //     .then(function (response) {
-    //         console.log(response);
-    //     })
-    //     .catch(function (error) {
-    //         console.log(error);
-    //     })
+  // axios.post('http://localhost:8000/post', {
+  //     "id": "5",
+  //     "text": "Test post",
+  //     "likes": "5"
+  // })
+  //     .then(function (response) {
+  //         console.log(response);
+  //     })
+  //     .catch(function (error) {
+  //         console.log(error);
+  //     })
 })
 
-module.exports = router;
+module.exports = router
